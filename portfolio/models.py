f93fbe23f5cbd08ebd1826c080b85c60971e3b0d
danivherk <danivherk@gmail.com>
2018-09-25 14:00:37 +0200

    homepage almost finished

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
 class portfolio(models.Model):
-        image = models.ImageField(upload_to= 'logos/')
+        image = models.ImageField(upload_to= 'images/')
         text = models.CharField(max_length=50)
+
+        def __str__(self):
+            return self.title
--- services/models.py
+++ services/models.py
@@ -1,6 +1,14 @@
 from django.db import models
 
+
+
 class Service(models.Model):
-    image = models.ImageField(upload_to= 'logos/')
+    image = models.ImageField(upload_to='logos/')
     title = models.CharField(max_length=50)
-    summary = models.CharField(max_lemght=200)
+    summary = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.title
+
+class Meta:
+    ordering = ['id']
