1e64ec7d90fdc23caa6c191c8b9de23916fcb931
danivherk <danivherk@gmail.com>
2018-11-22 13:53:29 +0100

    did a lot

--- videoediting/urls.py
+++ videoediting/urls.py
@@ -5,5 +5,9 @@ from . import views
 
 urlpatterns = [
     path('', views.editing, name='editing'),
+    path('reason', views.reason, name='reason'),
+    path('consq', views.consq, name='consq'),
+    path('onemoment', views.onemoment, name='onemoment'),
+    path('camera', views.camera, name='camera'),
 
 ]
--- videoediting/views.py
+++ videoediting/views.py
@@ -2,3 +2,15 @@ from django.shortcuts import render
 
 def editing(request):
     return render(request, 'videoediting/editing.html')
+
+def reason(request):
+    return render(request, 'videoediting/reason.html')
+
+def consq(request):
+    return render(request, 'videoediting/consq.html')
+
+def onemoment(request):
+    return render(request, 'videoediting/onemoment.html')
+
+def camera(request):
+    return render(request, 'videoediting/camera.html')
